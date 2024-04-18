1cd7f276ee1f9139f4538b25b89471464a507099
WCY-dt <834421194@qq.com>
2024-04-18 20:58:52 +0800

    Update robots.txt to disallow URLs with view query parameter Add react-favicon and react-router-dom dependencies to package.json Add clusters.css file with styling for clusters component Add deploy.yml file for GitHub Actions to build and deploy the app Remove websites.js file as it is no longer needed Update index.css to remove unnecessary line breaks Add nav.css file with styling for navigation component Add clusters.js file for clusters component Update App.css to change background color and padding Update App.js to add navigation and routes for different clusters

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import './css/App.css';
-import Websites from './js/websites';
+import './css/nav.css';
+import Cluster from './js/clusters';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+  localStorage.removeItem('colorMap');
+
+  const routes = {
+    "/web": "websites",
+    "/doc": "documents",
+    "/ins": "inspirations"
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Websites</h1>
-        <Websites />
-      </header>
-    </div>
+    <>
+      <nav className="App-nav">
+        <ul className="App-nav-list">
+          {Object.entries(routes).map(([path, element]) => (
+            <li className="App-nav-item"><a href={path}>{element}</a></li>
+          ))}
+        </ul>
+      </nav>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Routes>
+              <Route exact path="/" element={<Cluster dataKey="websites" />} />
+              {Object.entries(routes).map(([path, element]) => (
+                <Route path={path} element={<Cluster dataKey={element} />} />
+              ))}
+            </Routes>
+          </header>
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/js/linkCard.js
+++ src/js/linkCard.js
@@ -1,13 +1,90 @@
 import React from 'react';
+import '../css/linkCard.css';
 
-function LinkCard({ website }) {
+function isColorTooLight(color) {
+  const r = parseInt(color.substr(1, 2), 16);
+  const g = parseInt(color.substr(3, 2), 16);
+  const b = parseInt(color.substr(5, 2), 16);
+  const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+  return yiq > 200; // Change this value to adjust the lightness threshold
+}
+
+function getBackgroundColor(category) {
+  let colorMap = JSON.parse(localStorage.getItem('colorMap')) || {'default': '#d1ecf1'};
+
+  if (!colorMap[category]) {
+    let randomColor;
+    do {
+      randomColor = '#' + Math.floor(Math.random()*16000000).toString(16);
+    } while (isColorTooLight(randomColor));
+    colorMap[category] = randomColor;
+    localStorage.setItem('colorMap', JSON.stringify(colorMap));
+  }
+
+  return colorMap[category] || colorMap['default'];
+}
+
+function getContrastColor(color) {
+  const r = parseInt(color.substr(1, 2), 16);
+  const g = parseInt(color.substr(3, 2), 16);
+  const b = parseInt(color.substr(5, 2), 16);
+  const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+  return (yiq >= 128) ? 'black' : 'white';
+}
+
+function LinkCard({ item }) {
+  const backgroundColor = getBackgroundColor(item.category);
+  const textColor = getContrastColor(backgroundColor);
+
   return (
     <a
-      className="Websites-link"
-      href={website.url}
+      className="link-card"
+      href={item.url ? item.url : '/#'}
       target="_blank"
-      rel="noopener noreferrer">
-      {website.title}
+      rel="noopener noreferrer"
+      id={item.title}
+    >
+      <div className="link-card-left">
+        <div className="link-card-category" style={{ backgroundColor, color: textColor }}>
+          {item.category ? item.category : 'UNKNOWN'}
+        </div>
+        <div className="link-card-icon">
+          <img
+            src={`https://www.google.com/s2/favicons?sz=32&domain=${item.url ? new URL(item.url).hostname : 'ch3nyang.top'}`}
+            alt="favicon"
+            className="link-card-icon-img"
+          />
+        </div>
+      </div>
+      
+      <div className="link-card-right">
+        <div className="link-card-title">
+          {item.title}
+        </div>
+        <div className="link-card-description">
+          {item.description}
+        </div>
+        {item.detail ? (
+          <div className="link-card-detail">
+            {item.detail}
+          </div>
+        ) : null}
+        {item.links ? (
+          <div className="link-card-link">
+            {item.links && item.links.length > 0 ? item.links.map((link) => (
+              <a
+                key={link.url}
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="link-card-link-item"
+              >
+                {link.title}
+              </a>
+            )) : ''}
+          </div>
+        ) : null}
+      </div>
     </a>
   );
 }
