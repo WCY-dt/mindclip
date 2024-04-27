26a7832dc93616e5e7405a142c1a9bd20bc93982
WCY-dt <834421194@qq.com>
2024-04-27 18:52:34 +0800

    Refactor dataFetcher and fetchAndFilterData functions in dataFetcher.ts

--- src/services/dataFetcher.ts
+++ src/services/dataFetcher.ts
@@ -7,36 +7,26 @@ export interface ClusterProps {
   links: { title: string, url: string }[];
 }
 
-export function fetchData(
-  dataKey: string,
-  setClusters: (value: ClusterProps[]) => void
-) {
-  fetch('data.json')
-    .then(response => response.json())
-    .then(data => setClusters(data[dataKey]));
-}
-
-
 export function fetchAndFilterData(
   dataKey: string,
-  selectedCategory: string | null,
-  searchTerm: string,
+  selectedCategory: string | null = null,
+  searchTerm: string = '',
   setClusters: (value: ClusterProps[]) => void
 ) {
   fetch('data.json')
     .then(response => response.json())
     .then(data => {
-      let filteredClusters = data[dataKey];
+      let clusters = data[dataKey];
       if (selectedCategory) {
-        filteredClusters = filteredClusters.filter((cluster: ClusterProps) => cluster.category === selectedCategory);
+        clusters = clusters.filter((cluster: ClusterProps) => cluster.category === selectedCategory);
       }
       if (searchTerm) {
-        filteredClusters = filteredClusters.filter((cluster: ClusterProps) =>
+        clusters = clusters.filter((cluster: ClusterProps) =>
           Object.values(cluster).some(value =>
             value.toString().toLowerCase().includes(searchTerm.toLowerCase())
           )
         );
       }
-      setClusters(filteredClusters);
+      setClusters(clusters);
     });
 }
