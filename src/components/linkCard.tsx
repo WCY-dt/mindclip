9edead1c5265dfe93c115f60de6aad488b53c122
WCY-dt <834421194@qq.com>
2024-04-28 23:03:53 +0800

    Fix URL encoding issue in LinkCard component

--- src/components/linkCard.tsx
+++ src/components/linkCard.tsx
@@ -19,18 +19,18 @@ function LinkCard({ item, setSelectedCategory }: LinkCardProps) {
   return (
     <a
       className="link-card"
-      href={item.Urlpath ? item.Urlpath : '#'}
+      href={item.Urlpath ? item.Urlpath.replace(/&#39;/g, "'") : '#'}
       target="_blank"
       rel="noopener noreferrer"
       id={item.Title}
     >
       <div className="link-card-left">
         <div className="link-card-category" style={{ backgroundColor, color: textColor }} onClick={handleCategoryClick}>
-          {item.Category ? item.Category : 'UNKNOWN'}
+          {item.Category ? item.Category.replace(/&#39;/g, "'") : 'UNKNOWN'}
         </div>
         <div className="link-card-icon">
           <img
-            src={`https://www.google.com/s2/favicons?sz=64&domain=${item.Urlpath ? new URL(item.Urlpath).hostname : 'ch3nyang.top'}`}
+            src={`https://www.google.com/s2/favicons?sz=64&domain=${item.Urlpath ? new URL(item.Urlpath.replace(/&#39;/g, "'")).hostname : 'ch3nyang.top'}`}
             alt="favicon"
             className="link-card-icon-img"
           />
@@ -39,14 +39,14 @@ function LinkCard({ item, setSelectedCategory }: LinkCardProps) {
       
       <div className="link-card-right">
         <div className="link-card-title">
-          {item.Title}
+          {item.Title.replace(/&#39;/g, "'")}
         </div>
         <div className="link-card-description">
-          {item.Descr}
+          {item.Descr.replace(/&#39;/g, "'")}
         </div>
         {item.Detail ? (
           <div className="link-card-detail">
-            {item.Detail}
+            {item.Detail.replace(/&#39;/g, "'")}
           </div>
         ) : null}
         {item.links ? (
@@ -54,12 +54,12 @@ function LinkCard({ item, setSelectedCategory }: LinkCardProps) {
             {item.links && item.links.length > 0 ? item.links.map((link) => (
               <a
                 key={link.Urlpath}
-                href={link.Urlpath}
+                href={link.Urlpath.replace(/&#39;/g, "'")}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="link-card-link-item"
               >
-                {link.Title}
+                {link.Title.replace(/&#39;/g, "'")}
               </a>
             )) : ''}
           </div>
